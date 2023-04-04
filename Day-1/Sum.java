0b7eb843d55e5f3b6b779effc68b655266d9b481
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-04-04 21:22:40 +0530

    Day-1 programs done

---
+++ Day-1/Sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Sum
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter x value:");
+int x=sc.nextInt();
+System.out.println("enter y value:");
+int y=sc.nextInt();
+System.out.println(x+y);
+}
+}
