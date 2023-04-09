baca99f8c14bb474d36d038e07e054cd0aa43f15
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-04-09 10:19:19 +0530

    Day-6 programs done

---
+++ Day-6/EscapeCharacters.java
@@ -0,0 +1,9 @@
+class EscapeCharacters
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Mom\'s kitchen");
+        System.out.println("world's best\"Mom\"");
+        System.out.println("helo\\world");
+    }
+}
---
+++ Day-6/EscapeSequences.java
@@ -0,0 +1,12 @@
+class EscapeSequences
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello\nworld");
+        System.out.println("my self\fAruna");
+        System.out.println("ash\bwin");// removing left charcter
+        System.out.println("Mys\relf");// print right half if left are more the more will also print
+        System.out.println("my\tself");
+        
+    }
+}
