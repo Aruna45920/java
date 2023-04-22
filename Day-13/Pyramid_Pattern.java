cc655aa26a4040f7acaf4ddf17672a78a0853cf2
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-04-22 17:27:26 +0530

    Day-13 programs done

--- Day-13/Pyramid_Pattern.java
+++ Day-13/Pyramid_Pattern.java
@@ -5,13 +5,13 @@ public class Pyramid_Pattern
         int n=6;
         for(int i=0;i<n;i++)
         {
-            for(int j=n-i;j>1;j--)
+            for(int j=1;j<=n-i;j++)
             {
-                System.out.print("");
+                System.out.print(" ");
             }
-            for(int j=0;j<=i;j++)
+            for(int k=1;k<=i;k++)
             {
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
         }
