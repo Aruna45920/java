7bd9ea6272b405a1ac522619b406c9a018d7da43
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-04-27 17:46:54 +0530

    Day-14 programs done

---
+++ Day-14/Triangle_Character.java
@@ -0,0 +1,23 @@
+/*
+    A     
+   A B    
+  A B C   
+ A B C D  
+A B C D E 
+*/
+
+class Triangle_Character
+{
+    public static void main(String args[])
+    {
+        int n=5;
+        for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++)
+        System.out.print(" ");
+        char start='A';
+        for(int k=1;k<=i;k++)
+        System.out.print(start+++" ");
+        System.out.println();
+    }
+}
+}
