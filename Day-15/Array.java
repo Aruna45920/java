43eb3586b670e5446de12149e1686465dace381e
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-05-03 22:32:47 +0530

    Day-15 programs done

---
+++ Day-15/Array.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class Array
+{
+    public static void main(String[] args)
+    {
+        int[] age={1,6,8};
+        System.out.println("First element: " + age[0]);
+        System.out.println("Second element: " + age[1]);
+        System.out.println("Third element: " + age[2]);
+
+    }
+
+}
+    
+
---
+++ Day-16/Array1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Array1
+{
+    public static void main(String args[])
+    {
+        int[] age={5,7,8,54,65};
+        System.out.println("Using for loop");
+        for(int i=0;i<age.length;i++)
+        System.out.println(age[i]);
+    }
+}
