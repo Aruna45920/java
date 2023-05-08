f67a8ff2737a40704ba0a5738c3f882d32f772aa
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-05-08 21:44:02 +0530

    Day-18 programs done

---
+++ Day-18/MethodOverloading.java
@@ -0,0 +1,17 @@
+public class MethodOverloading {
+    
+        public void add(int a,int b) {
+            
+        }
+        public void add(int a,int b,int c) {
+            
+        }
+        public static void main(String[] args) {
+            MethodOverloading m=new MethodOverloading();
+            m.add(4,5);
+            m.add(7,8,9);
+           
+
+    }
+    
+}
