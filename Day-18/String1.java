13d7475ea31a752f7e2ecc4d5bd35c99e191f10f
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-05-11 20:32:15 +0530

    Day-18 programs done

---
+++ Day-18/String1.java
@@ -0,0 +1,9 @@
+public class String1 { 
+    public static void main(String[] args) {
+        String first="java";
+        String second="python";
+        System.out.println(first);
+        System.out.println(second);
+    }
+    
+}
