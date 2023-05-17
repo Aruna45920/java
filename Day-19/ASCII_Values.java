cb7b7681d3f5498e1fc47524df6737e2f35ea440
Aruna45920 <129878498+Aruna45920@users.noreply.github.com>
2023-05-17 14:25:56 +0530

    Day-19 programs done

---
+++ Day-19/ASCII_Values.java
@@ -0,0 +1,8 @@
+public class ASCII_Values {
+    public static void main(String[] args) {
+        int i;
+        for(i=0;i<=255;i++)
+        System.out.println("The ASCII valueof"+(char)i+"="+i+"\n");
+    }
+    
+}
